8a5b2836b91dd82798854798bb9161cfebc36179
Soukaina99255 <soukaina.illa@uit.ac.ma>
2023-03-17 07:35:22 -0700

    Print az

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char s;
+
+for (s = 'a'; s <= 'z'; ++s)
+	putchar(s);
+putchar('\n');
+return (0);
+}
