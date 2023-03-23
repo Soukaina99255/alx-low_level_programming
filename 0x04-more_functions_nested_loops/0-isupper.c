48a7621139890306352a5f65b626f81ae470a7ea
Soukaina99255 <soukaina.illa@uit.ac.ma>
2023-03-23 07:12:57 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,8 +2,8 @@
 
 /**
  * _isupper - checks for uppercase character
- * @c: input
- * Return: 1 if c is uppercase, 0 otherwise
+ * @c: character to be checked
+ * Return: 1 if true, 0 if false
  */
 
 int _isupper(int c)
@@ -11,6 +11,5 @@ int _isupper(int c)
 	if (c >= 'A' && c <= 'Z')
 		return (1);
 
-	else
-	       return (0);
+	return (0);
 }
