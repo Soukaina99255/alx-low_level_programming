062febb3770e198a4f683ed1f61d0798e5598757
Soukaina99255 <soukaina.illa@uit.ac.ma>
2023-04-11 13:13:07 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,15 +12,15 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	char *h;
+	char *s3;
 	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
 	while (s1 && s1[len1])
 		len1++;
 	while (s2 && s2[len2])
 		len2++;
-	h = malloc(sizeof(char) * (len1 + len2 + 1));
-	if (h == NULL)
+	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (s3 == NULL)
 		return (NULL);
 	i = 0;
 	j = 0;
@@ -29,12 +29,12 @@ char *str_concat(char *s1, char *s2)
 	{
 		while (i < (len1 + len2))
 		{
-			h[i] = s2[j];
+			s3[i] = s2[j];
 			i++;
 			j++;
 		}
 	}
-	h[i] = '\0';
+	s3[i] = '\0';
 
-	return (h);
+	return (s3);
 }
