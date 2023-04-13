4f34c64e47578b6a3c302ca5d0390874a1672eb8
Soukaina99255 <soukaina.illa@uit.ac.ma>
2023-04-13 09:44:56 -0700

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -7,18 +7,19 @@
  * @s2: string to concatenate from
  * @n: number of bytes from s2 to concatenates to s1
  *
- *Return: pointer to th resulting string
+ * Return: pointer to th resulting string
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s;
 
-	unsigned int i = 0, j = 0, len1 = 0, len2;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
 	while (s1 && s1[len1])
 
 		len1++;
+
 	while (s2 && s2[len2])
 
 		len2++;
