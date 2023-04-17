b7028a729185fa44b2b8eb4587d975ba3c088e2f
Soukaina99255 <soukaina.illa@uit.ac.ma>
2023-04-17 10:41:59 -0700

    task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,13 +7,16 @@
  * @name: name to initalize
  * @age: age to initalize
  * @owner: owner to intialize
+ *
+ * Return: void
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-		d = malloc(sizeof(struct dog));
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
