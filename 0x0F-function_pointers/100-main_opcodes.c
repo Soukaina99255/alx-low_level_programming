f7df23180772d20b1072595dedde6ae193825d3e
Soukaina99255 <soukaina.illa@uit.ac.ma>
2023-04-19 11:00:44 -0700

    task 100

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,42 +2,26 @@
 #include <stdlib.h>
 
 /**
- * main - prints its own opcodes
+ * main - check the code for the school students
  * @argc: number of argumments
- * @argv: array of arguments
+ * @argv: argument vector
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 
 int main(int argc, char *argv[])
 {
-	int bytes, i;
-	char *arr;
+	char *p = (char *)main;
+	int b;
 
 	if (argc != 2)
-	{
-		printf("Error\n");
-		exit(1);
-	}
+		printf("Error\n"), exit(1);
+	b = atoi(argv[1]);
 
-	bytes = atoi(argv[1]);
+	if (b < 0)
+		printf("Error\n"), exit(2);
 
-	if (bytes < 0)
-	{
-		printf("Error\n");
-		exit(2);
-	}
-
-	arr = (char *)main;
-
-	for (i = 0; i < bytes; i++)
-	{
-		if (i == bytes - 1)
-		{
-			printf("%02hhx\n", arr[i]);
-			break;
-		}
-		printf("%02hhx", arr[i]);
-	}
+	while (b--)
+		printf("%02hhx%s", *p++, b ? " " : "\n");
 	return (0);
 }
