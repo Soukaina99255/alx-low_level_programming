9a2e1570250e4135bf533523e5efb0f065a134a0
Soukaina99255 <soukaina.illa@uit.ac.ma>
2023-04-29 08:08:44 -0700

    task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
+	size_t s = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
-		num++;
+		s++;
 		h = h->next;
 	}
 
-	return (num);
+	return (s);
 }
