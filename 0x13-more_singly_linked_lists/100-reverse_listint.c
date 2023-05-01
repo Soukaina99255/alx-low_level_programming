2ae1eaed3aedbc21772f87438f348a6b66d00bb7
Soukaina99255 <soukaina.illa@uit.ac.ma>
2023-05-01 08:06:26 -0700

    task 100

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -3,8 +3,10 @@
 /**
  * reverse_listint - reverses a listint_t linked list
  * @head: head
+ *
  * Return: a pointer to the first node of the reversed list
  */
+
 listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *h = NULL, *s = NULL;
@@ -16,6 +18,7 @@ listint_t *reverse_listint(listint_t **head)
 		h = *head;
 		*head = s;
 	}
+
 	*head = s;
 	return (*head);
 }
